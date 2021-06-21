d965daf8738508e78439247156854ac0043fad39
shinre <ex559630@gmail.com>
2021-06-21 15:20:16 +0800

    c5-7

--- SimVillage.kt
+++ SimVillage.kt
@@ -5,5 +5,9 @@ fun main(){
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
-    println(greetingFunction("Guyal",2))
+    runSimulation("Guyal",greetingFunction)
+}
+fun runSimulation(playerName: String, greetingFunction: (String,Int) -> String){
+    val numBuildings = (1..3).shuffled().last()
+    println(greetingFunction(playerName,numBuildings))
 }
