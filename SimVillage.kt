a9efc1b4b5d24de35b8b63c6756f393b8572656e
shinre <ex559630@gmail.com>
2021-06-21 17:09:50 +0800

    c5-12

--- SimVillage.kt
+++ SimVillage.kt
@@ -1,11 +1,18 @@
 fun main(){
-    runSimulation("Guyal"){ playerName , numBuildings ->
+    runSimulation("Guyal",::printConstructionCost) { playerName , numBuildings ->
         val currentYear = 2021
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
 }
-inline fun runSimulation(playerName: String, greetingFunction :(String,Int) -> String){
+inline fun runSimulation(playerName: String,
+                         costPrinter: (Int) -> Unit,
+                         greetingFunction :(String,Int) -> String) {
     val numBuildings = (1..3).shuffled().last()
+    costPrinter(numBuildings)
     println(greetingFunction(playerName,numBuildings))
+}
+fun printConstructionCost(numBuilding:Int){
+    val cost = 500
+    println("Costruction cost: ${cost * numBuilding}")
 }
