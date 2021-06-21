b672a92a6a2fe6877a562693a02cef39c6112207
shinre <ex559630@gmail.com>
2021-06-21 14:19:05 +0800

    c5-2

--- SimVillage.kt
+++ SimVillage.kt
@@ -1,6 +1,7 @@
 fun main(){
-    print({
+    val greetingFunction:() -> String ={
         val currentYear = 2018
         "Welcome to SimVillage, Mayor! (copyright $currentYear)"
-    }())
+    }
+    println(greetingFunction())
 }
