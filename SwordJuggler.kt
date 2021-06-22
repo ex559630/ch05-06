8786eb73e6f19deb8f4a6fbfccfb12e95e333e8d
shinre <ex559630@gmail.com>
2021-06-22 14:38:39 +0800

    c6-11

---
+++ SwordJuggler.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var swordsJuggling: Int? =null
+    val isJugglingProficient =(1..3).shuffled().last() == 3
+    if (isJugglingProficient){
+        swordsJuggling = 2
+    }
+    println("You juggle $swordsJuggling swords!")
+}
